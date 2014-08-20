90441d4073ab662e00b4bd326713cb87cfec3986
Kent Fredric <kentfredric@gmail.com>
2014-08-21 03:23:07 +1200

    initial commit

---
+++ lib/Pod/Weaver/PluginBundle/Author/KENTNL.pm
@@ -0,0 +1,19 @@
+use 5.008;    # utf8
+use strict;
+use warnings;
+use utf8;
+
+package Pod::Weaver::PluginBundle::Author::KENTNL;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT:
+
+# AUTHORITY
+
+use Moose;
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
