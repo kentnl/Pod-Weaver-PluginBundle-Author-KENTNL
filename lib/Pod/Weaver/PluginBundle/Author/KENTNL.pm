476285594a12e310a4a7df15c658955cf11626e0
Kent Fredric <kentfredric@gmail.com>
2014-08-21 22:20:02 +1200

    Build results of 90441d4 (on master)

--- lib/Pod/Weaver/PluginBundle/Author/KENTNL.pm
+++ lib/Pod/Weaver/PluginBundle/Author/KENTNL.pm
@@ -7,14 +7,38 @@ package Pod::Weaver::PluginBundle::Author::KENTNL;
 
 our $VERSION = '0.001000';
 
-# ABSTRACT:
+# ABSTRACT: KENTNL's amazing Pod::Weaver Plugin Bundle.
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
-use Moose;
+use Moo qw( has with );
+with 'Pod::Weaver::PluginBundle::Author::KENTNL::Role::Easy';
 
-__PACKAGE__->meta->make_immutable;
-no Moose;
+sub bundle_prefix { '@A:KNL' }
+sub mvp_aliases { { command => qw[commands] } }
+sub mvp_multivalue_args { qw( commands ) }
+
+has 'commands' => (
+  is => ro =>,
+  predicate => 'has_commands',
+  lazy => 1,
+  default => sub { [] },
+);
+
+sub instance_config {
+  my ( $self ) = @_;
+  $self->inhale_bundle('@Author::KENTNL::Core');
+  $self->inhale_bundle('@Author::KENTNL::Prelude');
+  my (@config);
+  if ( $self->has_commands ) {
+    push @config, { payload => { 'commands' => $self->commands }};
+  }
+  $self->inhale_bundle('@Author::KENTNL::Commands',@config);
+  $self->inhale_bundle('@Author::KENTNL::Postlude');
+  return;
+}
+
+no Moo;
 
 1;
 
@@ -26,7 +50,7 @@ __END__
 
 =head1 NAME
 
-Pod::Weaver::PluginBundle::Author::KENTNL - # AUTHORITY
+Pod::Weaver::PluginBundle::Author::KENTNL - KENTNL's amazing Pod::Weaver Plugin Bundle.
 
 =head1 VERSION
 
